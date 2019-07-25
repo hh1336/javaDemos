2f45278924d73cda8da11d844a4b64de6a89fd64
myu <1435473873@qq.com>
2019-07-25 22:06:52 +0800

    test

--- WebDemos/maven_application/src/java/domain/User.java
+++ WebDemos/maven_application/src/java/domain/User.java
@@ -1,4 +1,13 @@
 package domain;
 
 public class User {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
